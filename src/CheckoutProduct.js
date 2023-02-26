2c830313e28c10b70798d08e630b2b8e0149c103
aryash19 <aryashchaudhary12341234@gmail.com>
2023-02-26 22:03:56 +0530

    new

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./checkoutProduct.css"
+
+function CheckoutProduct() {
+  return (
+    <div className="checkoutProduct">
+    <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" className="checkoutProduct__image" />
+
+    <div className="product__info">
+        <p className="checkoutProduct__title">
+        Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)
+        </p>
+        <p className="checkoutProduct__price">
+        <small>Rs.</small>
+                <strong>300</strong> 
+        </p>
+        <div className="checkoutProduct__rating">
+        <p>⭐⭐⭐</p>
+        </div>
+        <button>Remove from basket</button>
+    </div>
+      
+    </div>
+  )
+}
+
+export default CheckoutProduct
